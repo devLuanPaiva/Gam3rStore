c51b54424b04c79314a2d09538acbc93735073d7
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-18 09:17:59 -0300

    feat: create product controller

---
+++ apps/api/src/product/product.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { IProduct } from '@gstore/core';
+
+@Controller('products')
+export class ProductController {
+  constructor(private readonly repo: ProductService) {}
+  @Post('create')
+  save(@Body() product: IProduct): Promise<void> {
+    return this.repo.save(product);
+  }
+  @Get('getAll')
+  getAll(): Promise<IProduct[]> {
+    return this.repo.getProducts();
+  }
+  @Get('get/:id')
+  getById(@Param('id') id: number): Promise<IProduct> {
+    return this.repo.getById(id);
+  }
+  @Delete('delete/:id')
+  deleteById(@Param('id') id: number): Promise<void> {
+    return this.repo.deleteById(id);
+  }
+}
