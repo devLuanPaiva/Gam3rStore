f1c7aa2d233b807d104f76f319cb028a919cbe1a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-18 09:18:24 -0300

    feat: import product module in the app module

---
+++ apps/api/src/product/product.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
+import { DbModule } from 'src/db/db.module';
+
+@Module({
+  imports: [DbModule],
+  providers: [ProductService],
+  controllers: [ProductController],
+})
+export class ProductModule {}
