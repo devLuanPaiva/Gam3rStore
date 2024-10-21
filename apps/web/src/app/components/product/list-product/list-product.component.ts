d1b4d663a0795b4208376eeb2353064cceb5f13a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-21 16:15:44 -0300

    feat: create list products componet

---
+++ apps/web/src/app/components/product/list-product/list-product.component.ts
@@ -0,0 +1,20 @@
+import { IProduct } from '@gstore/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../../services/product.service';
+import { ItemProductComponent } from '../item-product/item-product.component';
+@Component({
+  selector: 'app-list-product',
+  standalone: true,
+  imports: [CommonModule, ItemProductComponent],
+  templateUrl: './list-product.component.html',
+})
+export class ListProductComponent {
+  products: IProduct[] = [];
+
+  constructor(private readonly productService: ProductService) {
+    this.productService.products$.subscribe((products) => {
+      this.products = products;
+    });
+  }
+}
