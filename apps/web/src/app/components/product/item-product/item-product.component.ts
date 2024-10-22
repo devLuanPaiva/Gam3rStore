944c9cc999321740760faeb57703bae1dd7eec2f
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-22 14:49:22 -0300

    feat: add rating component in item component

--- apps/web/src/app/components/product/item-product/item-product.component.ts
+++ apps/web/src/app/components/product/item-product/item-product.component.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { IProduct } from '@gstore/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {faCartShopping} from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { ItemReviewComponent } from '../item-review/item-review.component';
 @Component({
   selector: 'app-item-product',
   standalone: true,
-  imports: [CommonModule, RouterModule, FontAwesomeModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule, ItemReviewComponent],
   templateUrl: './item-product.component.html',
 })
 export class ItemProductComponent {
   @Input() product!: IProduct;
   icons = {
     cart: faCartShopping,
-  }
+  };
 }
