259e15248f8ea0995ad7cd2c8622e1d4c6412d4e
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-22 16:53:47 -0300

    feat: testing return all products

--- apps/api/src/product/product.spec.ts
+++ apps/api/src/product/product.spec.ts
@@ -6,30 +6,36 @@ import { mockProducts } from '../mocks/product.mock';
 
 describe('ProductController', () => {
   let controller: ProductController;
-  //   let service: ProductService;
-
   const mockProductService = {
-    save: jest.fn(),
-    getProducts: jest.fn(),
-    getById: jest.fn(),
-    deleteById: jest.fn(),
+    save: jest.fn() as jest.Mock<Promise<void>>,
+    getProducts: jest.fn() as jest.Mock<Promise<IProduct[]>>,
+    getById: jest.fn() as jest.Mock<Promise<IProduct | null>>,
+    deleteById: jest.fn() as jest.Mock<Promise<void>>,
   };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
-      providers: [ProductService],
-    })
-      .overrideProvider(ProductService)
-      .useValue(mockProductService)
-      .compile();
+      providers: [
+        {
+          provide: ProductService,
+          useValue: mockProductService,
+        },
+      ],
+    }).compile();
+
     controller = module.get<ProductController>(ProductController);
-    // service = module.get<ProductService>(ProductService);
   });
 
   it('should save a product', async () => {
     const product: IProduct = mockProducts[0];
     await controller.save(product);
     expect(mockProductService.save).toHaveBeenCalledWith(product);
+  });
+
+  it('should return all products', async () => {
+    const products: IProduct[] = mockProducts;
+    mockProductService.getProducts.mockResolvedValue(products);
+    expect(await controller.getAll()).toBe(products);
   });
 });
