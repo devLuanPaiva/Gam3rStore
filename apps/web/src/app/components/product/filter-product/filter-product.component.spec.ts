d3f06f8d296be2de9e860c532f0966352df62fc9
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-25 10:44:40 -0300

    feat: testing if set search value correctly when setSeach it called

--- apps/web/src/app/components/product/filter-product/filter-product.component.spec.ts
+++ apps/web/src/app/components/product/filter-product/filter-product.component.spec.ts
@@ -8,20 +8,28 @@ import { MockProductsService } from '../../../services/product.mock';
 describe('FilterProductComponent', () => {
   let component: FilterProductComponent;
   let fixture: ComponentFixture<FilterProductComponent>;
-  let productService: ProductService;
+  let productService: MockProductsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FilterProductComponent, FormsModule],
+      imports: [FilterProductComponent],
       providers: [{ provide: ProductService, useClass: MockProductsService }],
     }).compileComponents();
+
     fixture = TestBed.createComponent(FilterProductComponent);
     component = fixture.componentInstance;
-    productService = TestBed.inject(ProductService);
+    productService = TestBed.inject(
+      ProductService,
+    ) as unknown as MockProductsService;
     fixture.detectChanges();
   });
 
   it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+  it('should set seach value correctly when setSeach it called', () => {
+    const searchValue = 'notebook';
+    component.setSearch(searchValue);
+    expect(productService['searchValue']).toBe(searchValue);
   });
 });
