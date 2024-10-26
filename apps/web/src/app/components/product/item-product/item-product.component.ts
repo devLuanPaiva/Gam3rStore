548f609eea1936633c71ec929a7efe5981e8703f
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-26 14:44:36 -0300

    feat: add method to add item to cart

--- apps/web/src/app/components/product/item-product/item-product.component.ts
+++ apps/web/src/app/components/product/item-product/item-product.component.ts
@@ -5,6 +5,7 @@ import { IProduct } from '@gstore/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { ItemReviewComponent } from '../item-review/item-review.component';
+import { CartService } from '../../../services/cart.service';
 @Component({
   selector: 'app-item-product',
   standalone: true,
@@ -16,4 +17,9 @@ export class ItemProductComponent {
   icons = {
     cart: faCartShopping,
   };
+  constructor(private readonly cartService: CartService) {}
+  addToCart(product: IProduct) {
+    console.log('Adding product to cart: ', product);
+    this.cartService.addItem(product);
+  }
 }
