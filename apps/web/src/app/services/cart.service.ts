19156277e5353d4923cc5409f9eb6c138f86aa3d
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-26 10:10:52 -0300

    feat: create cart service

---
+++ apps/web/src/app/services/cart.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { LocalStorageService } from './local-storage.service';
+import {
+  CalculateInstallment,
+  Cart,
+  ICartItem,
+  IInstallment,
+  IProduct,
+} from '@gstore/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  private cart: Cart = new Cart();
+  constructor(private readonly localStorageService: LocalStorageService) {
+    this.loadCart();
+  }
+
+  get items(): ICartItem[] {
+    return this.cart.items;
+  }
+
+  get totalQuantity(): number {
+    return this.cart.quantityItems;
+  }
+
+  get totalPrice(): number {
+    return this.cart.totalPrice;
+  }
+  get fullPrice(): number {
+    return this.cart.fullPrice;
+  }
+  get installment(): IInstallment {
+    return new CalculateInstallment().execute(this.cart.totalPrice);
+  }
+  addItem(product: IProduct): void {
+    this.updateCart(this.cart.addItem(product));
+  }
+  removeItem(product: IProduct): void {
+    this.updateCart(this.cart.removeItem(product));
+  }
+
+  removeProduct(product: IProduct): void {
+    this.updateCart(this.cart.removeProduct(product));
+  }
+
+  clearCart(): void {
+    this.updateCart(this.cart.clear());
+  }
+
+  private loadCart(): void {
+    const savedItems = this.localStorageService.getItem<ICartItem[]>('cart');
+    if (savedItems) {
+      this.cart = new Cart(savedItems);
+    }
+  }
+  private updateCart(cart: Cart): void {
+    this.localStorageService.saveItem('cart', this.cart.items);
+    this.cart = cart;
+  }
+}
