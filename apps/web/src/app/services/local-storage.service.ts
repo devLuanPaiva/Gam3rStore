e3392200bcdce99eb0cb0323962f7ce600406659
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-28 10:21:07 -0300

    refactor: ensure local storage is accessed if available

--- apps/web/src/app/services/local-storage.service.ts
+++ apps/web/src/app/services/local-storage.service.ts
@@ -1,17 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocalStorageService {
+  constructor(@Inject(PLATFORM_ID) private readonly platformId: Object) {}
+
   saveItem(key: string, value: any): void {
-    localStorage.setItem(key, JSON.stringify(value));
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
   }
+
   getItem<T>(key: string): T | null {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : null;
+    if (isPlatformBrowser(this.platformId)) {
+      const item = localStorage.getItem(key);
+      try {
+        return item ? JSON.parse(item) : null;
+      } catch (error) {
+        console.error('Error parsing JSON from localStorage', error);
+        return null;
+      }
+    }
+    return null; // Ou outro valor padrão
   }
+
   removeItem(key: string): void {
-    localStorage.removeItem(key);
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem(key);
+    }
+  }
+
+  clear(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.clear();
+    }
   }
 }
