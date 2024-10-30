46a3749798c0ca262a2fe5cc42f4f207cdf14bc8
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-30 16:57:34 -0300

    feat: crate order module and  add in app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { DbModule } from './db/db.module';
 import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [DbModule, ProductModule],
+  imports: [DbModule, OrderModule, ProductModule],
   controllers: [AppController],
 })
 export class AppModule {}
