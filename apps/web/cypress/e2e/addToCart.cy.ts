1d159a146adb524e4304bde005ee0f83a3703c88
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-10-30 09:44:15 -0300

    feat: testing verify the number of cart items and add one product to cart

--- apps/web/cypress/e2e/addToCart.cy.ts
+++ apps/web/cypress/e2e/addToCart.cy.ts
@@ -3,8 +3,26 @@ describe('Add product to cart', () => {
     cy.intercept('GET', '**/products/getAll').as('getProducts');
   });
 
-  it('should verify quantity of cart items and add product', () => {
+  it('should verify products list are visible', () => {
     cy.visit('/');
     cy.get('app-list-product').should('be.visible');
+  });
+  it('should verify the number of cart items and add product', () => {
+    cy.visit('/');
+    // verify the number of cart items
+    cy.get('app-cart-icon div')
+      .invoke('text')
+      .then((initialAmount) => {
+        // add on product to cart
+        cy.get('app-item-product button').first().click();
+        // verify the number of cart items increased
+        cy.get('app-cart-icon div')
+          .invoke('text')
+          .should((updatedAmount) => {
+            expect(parseInt(updatedAmount)).to.be.greaterThan(
+              parseInt(initialAmount),
+            );
+          });
+      });
   });
 });
