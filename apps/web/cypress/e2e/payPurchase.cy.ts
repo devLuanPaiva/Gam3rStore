a64154ee322ceb584dd4632b22733409f97ec988
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-04 15:53:51 -0300

    feat: testing fill form delivery

--- apps/web/cypress/e2e/payPurchase.cy.ts
+++ apps/web/cypress/e2e/payPurchase.cy.ts
@@ -1,13 +1,27 @@
 describe('Pay Purchase', () => {
   before(() => {
     cy.intercept('GET', '**/products/getAll').as('getProducts');
+    cy.intercept('POST', '**/orders/create').as('createOrder');
   });
-  it('should add product to cart and navigate for payment page', () => {
-    cy.visit('/')
+  it('should pay purchase', () => {
+    cy.visit('/');
+    //should add product to cart
     cy.get('app-item-product button').first().click();
     cy.get('app-cart-icon div').click();
     cy.url().should('include', '/cart');
+    // navigate for payment page
     cy.get('app-cart-total a').first().click();
     cy.url().should('include', '/cart/payment');
-  })
+
+    //should fill the form delivery:
+    cy.get('app-form-delivery input[name="name"]').type('João da Silva');
+    cy.get('app-form-delivery input[name="email"]').type(
+      'joao.silva@example.com',
+    );
+    cy.get('app-form-delivery input[name="cpf"]').type('12345678901');
+    cy.get('app-form-delivery input[name="street"]').type('Rua das Flores');
+    cy.get('app-form-delivery input[name="complement"]').type('Apt 101');
+    cy.get('app-form-delivery input[name="city"]').type('São Paulo');
+    cy.get('app-form-delivery input[name="state"]').type('SP');
+  });
 });
