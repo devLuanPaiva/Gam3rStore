659a50b89871d34f352c3dc2cc65ee08d4a95664
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-05 15:03:44 -0300

    feat: create product title component

---
+++ apps/web/src/app/components/product/product-title/product-title.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { IProduct } from '@gstore/core';
+@Component({
+  selector: 'app-product-title',
+  standalone: true,
+  imports: [],
+  template: `
+    <section class="flex flex-col">
+      <h1 class="text-lg sm:text-xl md:text-2xl lg:text-3xl">
+        {{ product.name }}
+      </h1>
+      <p class="text-sm sm:text-base md:text-lg font-light text-zinc-400">
+        {{ product.description }}
+      </p>
+    </section>
+  `,
+})
+export class ProductTitleComponent {
+  @Input() product!: IProduct;
+}
