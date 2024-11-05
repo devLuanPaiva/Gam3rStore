9098674214c9836f4c715760d6eac43213b54d8a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-05 16:35:31 -0300

    feat: create purchase banner component

---
+++ apps/web/src/app/components/product/purchase-banner/purchase-banner.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IInstallment, IProduct } from '@gstore/core';
+import { CartService } from '../../../services/cart.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faCreditCard,
+  faShoppingCart,
+} from '@fortawesome/free-solid-svg-icons';
+import { InstallmentService } from '../../../services/installment.service';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-purchase-banner',
+  standalone: true,
+  imports: [RouterModule, FontAwesomeModule, CommonModule],
+  templateUrl: './purchase-banner.component.html',
+})
+export class PurchaseBannerComponent {
+  @Input() product!: IProduct;
+  installmentDetails!: IInstallment;
+  icons = {
+    creditCard: faCreditCard,
+    cartShopping: faShoppingCart,
+  };
+  constructor(
+    private readonly cartService: CartService,
+    private readonly installmentService: InstallmentService,
+  ) {}
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['product'] && this.product.promotionalPrice) {
+      this.calculateInstallments();
+    }
+  }
+  calculateInstallments() {
+    if (this.product.promotionalPrice) {
+      this.installmentDetails = this.installmentService.calculateInstallment(
+        this.product.promotionalPrice,
+        12,
+      );
+    }
+  }
+  addItem(product: IProduct) {
+    this.cartService.addItem(product);
+  }
+}
