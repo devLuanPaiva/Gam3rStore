681346ba6785301cd0cba108a3a76654e0bc94b6
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-07 13:46:16 -0300

    refactor: justify and indent text

--- apps/web/src/app/components/product/product-title/product-title.component.ts
+++ apps/web/src/app/components/product/product-title/product-title.component.ts
@@ -9,7 +9,7 @@ import { IProduct } from '@gstore/core';
       <h1 class="text-lg sm:text-xl md:text-2xl lg:text-3xl">
         {{ product.name }}
       </h1>
-      <p class="text-sm sm:text-base md:text-lg font-light text-zinc-400">
+      <p class="text-sm sm:text-base md:text-lg font-light text-zinc-400 text-justify indent-8">
         {{ product.description }}
       </p>
     </section>
