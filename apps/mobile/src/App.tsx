15b131bd617880b872eb35bfe5b396d59384723b
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-12 14:35:39 -0300

    refactor: wrap aplication with CartProvider

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -2,15 +2,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import { ProductProvider } from "./data/contexts/ProductContext";
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Main from "./screens/Main";
+import { CartProvider } from "./data/contexts/CartContext";
 const Stack = createNativeStackNavigator()
 export default function App() {
-    return(
-    <NavigationContainer>
-        <ProductProvider>
-            <Stack.Navigator initialRouteName="Home" >
-                <Stack.Screen name='main' component={Main} />
-            </Stack.Navigator>
-        </ProductProvider>
-    </NavigationContainer>
+    return (
+        <NavigationContainer>
+            <CartProvider>
+                <ProductProvider>
+                    <Stack.Navigator initialRouteName="Home" >
+                        <Stack.Screen name='main' component={Main} />
+                    </Stack.Navigator>
+                </ProductProvider>
+            </CartProvider>
+        </NavigationContainer>
     )
 }
