389400dfd637baa384e462919102a38bde789787
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-16 08:06:24 -0300

    feat: configure stack for payment screen

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,8 +1,9 @@
+import Main from "./screens/Main";
+import Payment from "./screens/Payment";
+import { CartProvider } from "./data/contexts/CartContext";
 import { NavigationContainer } from "@react-navigation/native";
 import { ProductProvider } from "./data/contexts/ProductContext";
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Main from "./screens/Main";
-import { CartProvider } from "./data/contexts/CartContext";
 const Stack = createNativeStackNavigator()
 export default function App() {
     return (
@@ -10,7 +11,20 @@ export default function App() {
             <CartProvider>
                 <ProductProvider>
                     <Stack.Navigator initialRouteName="Home" >
-                        <Stack.Screen name='main' component={Main} />
+                        <Stack.Screen name='main' component={Main} options={{
+                            headerShown: false,
+                        }} />
+                        <Stack.Screen
+                            name="Pagamento"
+                            component={Payment}
+                            options={{
+                                title: 'Detalhes do Pagamento',
+                                headerBackTitle: 'Voltar',
+                                headerShown: true,
+                                headerStyle: { backgroundColor: '#0D001E' },
+                                headerTintColor: '#FFF',
+                            }}
+                        />
                     </Stack.Navigator>
                 </ProductProvider>
             </CartProvider>
