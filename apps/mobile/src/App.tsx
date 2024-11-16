7cb1034a318c3050fffed3a793902bb2e3978d20
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-16 15:10:03 -0300

    feat: write with PaymentProvider

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -4,28 +4,29 @@ import { CartProvider } from "./data/contexts/CartContext";
 import { NavigationContainer } from "@react-navigation/native";
 import { ProductProvider } from "./data/contexts/ProductContext";
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { PaymentProvider } from "./data/contexts/PaymentContext";
 const Stack = createNativeStackNavigator()
 export default function App() {
     return (
         <NavigationContainer>
             <CartProvider>
                 <ProductProvider>
-                    <Stack.Navigator initialRouteName="Home" >
-                        <Stack.Screen name='main' component={Main} options={{
-                            headerShown: false,
-                        }} />
-                        <Stack.Screen
-                            name="Pagamento"
-                            component={Payment}
-                            options={{
-                                title: 'Detalhes do Pagamento',
-                                headerBackTitle: 'Voltar',
-                                headerShown: true,
-                                headerStyle: { backgroundColor: '#0D001E' },
-                                headerTintColor: '#FFF',
-                            }}
-                        />
-                    </Stack.Navigator>
+                    <PaymentProvider>
+                        <Stack.Navigator initialRouteName="Main">
+                            <Stack.Screen name="Main" component={Main} options={{ headerShown: false }} />
+                            <Stack.Screen
+                                name="Payment"
+                                component={Payment}
+                                options={{
+                                    title: 'Detalhes do Pagamento',
+                                    headerBackTitle: 'Voltar',
+                                    headerShown: true,
+                                    headerStyle: { backgroundColor: '#0D001E' },
+                                    headerTintColor: '#FFF',
+                                }}
+                            />
+                        </Stack.Navigator>
+                    </PaymentProvider>
                 </ProductProvider>
             </CartProvider>
         </NavigationContainer>
