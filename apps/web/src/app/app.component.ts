8cec7bc97bb4ed8f5e606693de9832010f7c2016
Luan Alves <teste.checklist@nossoatacarejo.com.br>
2025-04-29 11:16:53 -0300

    feat: implement on init to call ga service

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { PageComponent } from './components/template/page/page.component';
+import { GoogleAnalyticsService } from './services/google-analytics.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PageComponent],
+  imports: [PageComponent],
   templateUrl: './app.component.html',
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly gaService = inject(GoogleAnalyticsService)
+
+  ngOnInit(): void {
+    this.gaService.load();
+  }
+}
